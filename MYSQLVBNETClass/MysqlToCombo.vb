f699b24a611a735df385125f70a6ab425e133446
tutorial88 <38849092+tutorial88@users.noreply.github.com>
2018-05-04 15:28:45 +0700

    Add files via upload

---
+++ MYSQLVBNETClass/MysqlToCombo.vb
@@ -0,0 +1,27 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class MysqlToCombo
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DataToCOmbo()
+    End Sub
+    Sub DataToCOmbo()
+        If OpenDB() Then
+
+            Try
+                Dim Query As String = "Select * from Country"
+                Dim CMD As New MySqlCommand(Query, Conn)
+                Dim DTreader As MySqlDataReader
+
+                DTreader = CMD.ExecuteReader
+                ComboBox1.Items.Clear()
+
+                While DTreader.Read
+                    ComboBox1.Items.Add(DTreader.GetString(1))
+                End While
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                Conn.Close()
+            End Try
+        End If
+    End Sub
+End Class
